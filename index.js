b7153933ce567da81227f3d93929c0d1e0393734
Chloe Gell <gellert.chloe@gmail.com>
2020-02-12 17:38:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Add your Circle class here
+
+class Circle {
+  constructor(radius){
+    this.radius = radius; 
+  }
+  get diameter(){
+    return 2*radius; 
+  }
+  get circumference(){
+    
+  }
+  get area(){
+    
+  }
+}
